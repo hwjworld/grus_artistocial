e26e43ec4f83930099819f34043f5abcbf27cd6c
Bokai Zhang <769043056@qq.com>
2022-10-25 13:20:20 +1000

    fix signup page

---
+++ websrc/layout/signupTop.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="../css/initialize.css">
+<link rel="stylesheet" href="../css/publicStyle.css">
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+
+<div class="top-tab menu background-color-f4Cf0a">
+    <div class="top-left-info">
+        <a class="home top-tab-item display-flex-center" onclick="top.location.href='../index.php'">HOME</a>
+        <a class="profile top-tab-item display-flex-center" onclick="top.location.href='../profile.php'">PROFILE</a>
+        <a class="library top-tab-item display-flex-center" onclick="top.location.href='../library.php'">LIBRARY</a>
+        <a class="events top-tab-item display-flex-center" onclick="top.location.href='../events.php'">EVENTS</a>
+        <a class="about top-tab-item display-flex-center" onclick="top.location.href='../about.php'">ABOUT</a>
+    </div>
+    <div class="top-right-sigin"><span class="top-right-signin-a">THIS IS THE SIGN UP PAGE</span></div>
+</div>
+
+<script>
+
+</script>
--- websrc/signup.php
+++ websrc/signup.php
@@ -5,13 +5,16 @@
     <link href="https://fonts.googleapis.com/css?family=JejuGothic&display=swap" rel="stylesheet" />
     <title>DECO1800/7180 Artistocial Login</title>
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/initialize.css">
+    <link rel="stylesheet" href="css/publicStyle.css">
+    <link rel="stylesheet" href="css/signin.css">
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <div class="Login_style">
+    <!-- <div class="Login_style">
         <div class="top_style"></div>
     
         <div class="background_style"></div>
@@ -37,6 +40,45 @@
                 </div>
                 <div class="figure_eight"></div>
             </div>
+            <div class="figure_eye_one"></div>
+            <div class="figure_eye_two"></div>
+            <div class="figure_arm"></div>
+            <div class="rightHandLogin"></div>
+            <div class="squre_style">
+                <div class="figure_nine"><span class="sentense_style">Nice to meet you!</span></div>
+                <div class="name"></div>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="content-info">
+        
+        <iframe frameborder="no" scrolling="no" class="w-100-iframe top-iframe" src="layout/signupTop.php?color=000000"></iframe>
+    
+        <div class="logo-image">
+            <img src="images/artistocial.png" width="230" height="100">
+        </div>
+        <div class="yellow-background"></div>
+        <span class="top-signin">SIGN UP</span>
+        <form action="../be/pgreq/signin.php" method="POST" id="login-form">
+        <input type="email" class="email"></div>
+        <input type="password" class="password"></div>
+        <div class="button_style"></div><span class="word_style">Bio:</span>
+        <span class="email_style">Occupation:</span>
+        <input type="text" class="lastname"></div>
+        <input type="text" class="occupation"></div><span class="occupation_style">Name:</span>
+        <span class="lastname_style">Password:</span>
+        <input type="text" class="firstName"></div><span class="firstname_style">Email Address(ID):</span>
+        <span class="sign_up">SIGN UP</span><a href="loginComplete.html" class="continue">Continue</a>
+
+        <div class="figure_one">
+            <div class="figure_two"></div>
+            <div class="figure_three"></div>
+            <div class="figure_four"></div>
+            <div class="figure_five">
+                <div class="figure_six"></div>
+            </div>
+
             <div class="figure_eye_one"></div>
             <div class="figure_eye_two"></div>
             <div class="figure_arm"></div>
