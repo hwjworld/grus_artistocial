a979dfb071a9078f9b4277148230206fad3db077
Jerome Huang <canni3269@gmail.com>
2022-10-25 12:47:26 +1000

    fix top profile.php link

--- websrc/layout/signinTop.php
+++ websrc/layout/signinTop.php
@@ -7,8 +7,8 @@
 <div class="top-tab menu background-color-f4Cf0a">
     <div class="top-left-info">
         <a class="home top-tab-item display-flex-center" onclick="top.location.href='../index.php'">HOME</a>
-        <a class="profile top-tab-item display-flex-center" onclick="top.location.href='../profile.html'">PROFILE</a>
-        <a class="library top-tab-item display-flex-center" onclick="top.location.href='../library.html'">LIBRARY</a>
+        <a class="profile top-tab-item display-flex-center" onclick="top.location.href='../profile.php'">PROFILE</a>
+        <a class="library top-tab-item display-flex-center" onclick="top.location.href='../library.php'">LIBRARY</a>
         <a class="events top-tab-item display-flex-center" onclick="top.location.href='../events.php'">EVENTS</a>
         <a class="about top-tab-item display-flex-center" onclick="top.location.href='../about.php'">ABOUT</a>
     </div>
